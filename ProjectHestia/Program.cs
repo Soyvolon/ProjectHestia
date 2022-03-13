fadc11247b5180437cc00ac96160359b64c58a0e
Bounds <andybounds@gmail.com>
2022-03-13 17:29:16 -0400

    Project files

---
+++ ProjectHestia/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Extensions.Logging.EventLog;
+
+using ProjectHestia;
+using ProjectHestia.Data;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args)
+#pragma warning disable CA1416 // Validate platform compatibility
+        => Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration(config =>
+            {
+                config.AddJsonFile(Path.Join("Config", "website_config.json"));
+            })
+            .ConfigureServices((hostContext, services) =>
+            {
+                try
+                {
+                    services.Configure<EventLogSettings>(settings =>
+                    {
+                        settings.SourceName = "Project Hestia";
+                    });
+                }
+                catch { /* not on windows */ }
+            })
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            })
+            .UseWindowsService();
+#pragma warning restore CA1416 // Validate platform compatibility
+}
---
+++ ProjectHestia/Startup.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.HttpOverrides;
+
+namespace ProjectHestia;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // Add services to the container.
+        services.AddRazorPages();
+        services.AddServerSideBlazor();
+
+
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        // Configure the HTTP request pipeline.
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            // app.UseMigrationsEndPoint();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseForwardedHeaders(new ForwardedHeadersOptions()
+        {
+            ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+        });
+
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+            endpoints.MapBlazorHub();
+            endpoints.MapFallbackToPage("/_Host");
+        });
+    }
+}
