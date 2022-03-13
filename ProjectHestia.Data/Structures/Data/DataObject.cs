cced115626913d6d3feffbe3bbb128e70a567715
Bounds <andybounds@gmail.com>
2022-03-13 17:37:20 -0400

    started file creation.

---
+++ ProjectHestia.Data/Structures/Data/DataObject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectHestia.Data.Structures.Data;
+public class DataObject<T> where T : notnull
+{
+    public T Key { get; set; }
+    public DateTime LastEdit { get; set; }
+}
